cd623148355867683e256173e404f956aebe87ab
Daniel-Butt <47188958+Daniel-Butt@users.noreply.github.com>
2023-12-26 12:32:25 -0500

    Added Lookup table generation

--- NumericOptimize.h
+++ NumericOptimize.h
@@ -7,7 +7,7 @@
 class NumericOptimize {
 
 private:
-    static double gold_minimize(std::function<double(double)> f, std::vector<double> I, const double tol = 1e-7) {
+    static double goldMinimize(std::function<double(double)> f, std::vector<double> I, const double tol = 1e-7) {
 
         const double phi = (sqrt(5.0) + 1.0) / 2.0; // Golden ratio
         double a = I[0];
@@ -28,32 +28,13 @@ private:
         return (b + a) / 2.0;
     }
 
-    static double brent_minimize(std::function<double(double)> f, std::vector<double> I, const double t = 1e-7) {
+    static double brentMinimize(std::function<double(double)> f, std::vector<double> I, const double t = 1e-7) {
         double a = I[0];
         double b = I[1];
-        double c;
-        double d;
-        double e;
-        double eps;
-        double fu;
-        double fv;
-        double fw;
-        double fx;
-        double m;
-        double p;
-        double q;
-        double r;
-        double sa;
-        double sb;
-        double t2;
-        double tol;
-        double u;
-        double v;
-        double w;
-        double x;
-        //
-        //  C is the square of the inverse of the golden ratio.
-        //
+
+        double c, d, e, eps, fu, fv, fw, fx, m, p, q, r, sa, sb, t2, tol, u, v, w, x;
+
+        // C is the square of the inverse of the golden ratio.
         c = 0.5 * (3.0 - sqrt(5.0));
 
         eps = sqrt(2.220446049250313E-016);
@@ -63,6 +44,7 @@ private:
         x = sa + c * (b - a);
         w = x;
         v = w;
+        d = 0.0;
         e = 0.0;
         fx = f(x);
         fw = fx;
@@ -72,16 +54,14 @@ private:
             m = 0.5 * (sa + sb);
             tol = eps * fabs(x) + t;
             t2 = 2.0 * tol;
-            //
-            //  Check the stopping criterion.
-            //
+
+            // Check the stopping criterion.
             if (fabs(x - m) <= t2 - 0.5 * (sb - sa))
             {
                 break;
             }
-            //
-            //  Fit a parabola.
-            //
+
+            // Fit a parabola.
             r = 0.0;
             q = r;
             p = q;
@@ -102,14 +82,12 @@ private:
             }
 
             if (fabs(p) < fabs(0.5 * q * r) && q * (sa - x) < p && p < q * (sb - x)) {
-                //
-                //  Take the parabolic interpolation step.
-                //
+
+                // Take the parabolic interpolation step.
                 d = p / q;
                 u = x + d;
-                //
-                //  F must not be evaluated too close to A or B.
-                //
+
+                // F must not be evaluated too close to A or B.
                 if ((u - sa) < t2 || (sb - u) < t2) {
                     if (x < m) {
                         d = tol;
@@ -119,9 +97,8 @@ private:
                     }
                 }
             }
-            //
-            //  A golden-section step.
-            //
+
+            // A golden-section step.
             else {
                 if (x < m) {
                     e = sb - x;
@@ -131,9 +108,8 @@ private:
                 }
                 d = c * e;
             }
-            //
-            //  F must not be evaluated too close to X.
-            //
+
+            // F must not be evaluated too close to X.
             if (tol <= fabs(d)) {
                 u = x + d;
             }
@@ -145,9 +121,8 @@ private:
             }
 
             fu = f(u);
-            //
-            //  Update A, B, V, W, and X.
-            //
+
+            // Update A, B, V, W, and X.
             if (fu <= fx) {
                 if (u < x) {
                     sb = x;
@@ -187,19 +162,20 @@ private:
         return x;
     }
 
-    static double brent_dekker_root(std::function<double(double)> func, std::vector<double> I, const double tol = 1e-7) {
+    static double brentDekkerRoot(std::function<double(double)> func, std::vector<double> I, const double tol = 1e-7) {
         int iter;
 
-        double a = I[0], b = I[1], c = I[1], d, e, min1, min2;
+        double a = I[0], b = I[1], c = I[1], d = 0.0, e = 0.0, min1, min2;
         double fa = func(a), fb = func(b), fc, p, q, r, s, tol1, xm;
 
-        if ((fa > 0.0 && fb > 0.0) || (fa < 0.0 && fb < 0.0))
-            return b;
+        //not bracketed
+        if (fa * fb > 0.0) return b;
 
         fc = fb;
 
         for (iter = 1; iter <= 1000; iter++) {
-            if ((fb > 0.0 && fc > 0.0) || (fb < 0.0 && fc < 0.0)) {
+            // if sign(fb) = sign(fc)
+            if (fb * fc > 0.0) {
                 c = a;
                 fc = fa;
                 e = d = b - a;
@@ -233,6 +209,7 @@ private:
                 }
 
                 if (p > 0.0) q = -q;
+
                 p = fabs(p);
 
                 min1 = 3.0 * xm * q - fabs(tol1 * q);
@@ -270,11 +247,11 @@ private:
 public:
 
     static double findRoot(std::function<double(double)> func, std::vector<double> I, const double tol = 1e-7) {
-        return brent_dekker_root(func, I, tol);
+        return brentDekkerRoot(func, I, tol);
     }
 
     static std::array<double, 2> minimize(std::function<double(double)> func, std::vector<double> I, const double tol = 1e-7) {
-        const double minX = brent_minimize(func, I, tol);
+        const double minX = brentMinimize(func, I, tol);
 
         return { minX, func(minX) };
     }
---
+++ OLS.h
@@ -0,0 +1,170 @@
+#pragma once
+
+#include <vector>
+#include <array>
+#include <iostream>
+#include <cmath>
+#include <functional>
+#include "NumericOptimize.h"
+
+
+class OLS {
+
+private:
+
+    static std::array<double, 2> fitLine(const double x1, const double x2, const double y1, const double y2) {
+
+        //y = mx + b
+        const double m = (y2 - y1) / (x2 - x1);
+        const double b = y1 - x1 * m;
+
+        return { m, b };
+    }
+
+    static double splineWorstCaseError(std::function<double(double)> func, std::vector<double> I) {
+
+        if (fabs(I[1] - I[0]) < 1e-7) return 0;
+
+        const auto& [m, b] = fitLine(I[0], I[1], func(I[0]), func(I[1]));
+
+        //negate function to make maximum the minimum
+        auto min_func = [&](double x) { return -abs(func(x) - (m * x + b)); };
+
+        //negate output to make minimum the original maximum
+        return -NumericOptimize::minimize(min_func, I)[1];
+    }
+
+    inline static std::function<double(double)> kmtempFunc;
+    inline static double kmtempLbound;
+    inline static double kmtempEps;
+
+    static double knotRootFunc(double x) {
+        return splineWorstCaseError(kmtempFunc, { kmtempLbound, x }) - kmtempEps;
+    }
+
+    static double findNextKnot(std::function<double(double)> func, std::vector<double> I, double eps, double tol = 1e-7) {
+
+        kmtempLbound = I[0];
+
+        return NumericOptimize::findRoot(knotRootFunc, I, tol);
+    }
+
+    static std::vector<double> olsMinmax(std::function<double(double)> func, std::vector<double> I, double eps, double tol) {
+
+        kmtempFunc = func;
+        kmtempEps = eps;
+
+        double currentKnot = I[0];
+
+        std::vector<double> knots = { currentKnot };
+
+        while (I[1] - currentKnot > tol) {
+
+            currentKnot = findNextKnot(func, { currentKnot, I[1] }, eps, tol);
+            knots.push_back(currentKnot);
+
+        }
+
+        return knots;
+    }
+
+    static std::pair<double, std::vector<double>> olsRefinedMinmax(std::function<double(double)> func, std::vector<double> I, double eps, double tol) {
+
+        //binary search to refine epsilon (max error) value
+        double lEps = 0.0;
+        double uEps = eps;
+        double currentEps = eps;
+        double lastEps = 0.0;
+        int minKnots = (int)olsMinmax(func, I, eps, tol).size();
+
+        while (abs(currentEps - lastEps) > tol) {
+
+            lastEps = currentEps;
+            currentEps = (lEps + uEps) / 2.0;
+            const int num_knots = (int)olsMinmax(func, I, currentEps, tol).size();
+
+            if (num_knots > minKnots) {
+                lEps = currentEps;
+            }
+            else {
+                uEps = currentEps;
+            }
+        }
+
+        return { uEps, olsMinmax(func, I, uEps, tol) };
+    }
+
+
+public:
+
+    enum olsMethod {
+        standard,
+        refined
+    };
+
+    struct LineNode
+    {
+        double minX;
+        double maxX;
+        double m;
+        double b;
+    };
+
+    static std::pair<double, std::vector<double>> fitSpline(std::function<double(double)> func, std::vector<double> I, const olsMethod method = standard, const double eps = 0.01, const double tol = 1e-7) {
+
+        switch (method) {
+
+            case standard:
+                return { eps, olsMinmax(func, I, eps, tol) };
+
+            case refined:
+                return olsRefinedMinmax(func, I, eps, tol);
+
+            default:
+                return { eps, olsMinmax(func, I, eps, tol) };
+        }
+    }
+
+    static std::vector<LineNode> constructSplineTable(std::function<double(double)> func, std::vector<double>& knots) {
+
+        const int N = (int)knots.size() - 1;
+
+        std::vector<LineNode> lines;
+        lines.reserve(N);
+
+        for (int i = 0; i < N; i++) {
+
+            const auto& [m, b] = fitLine(knots[i], knots[i + 1], func(knots[i]), func(knots[i + 1]));
+
+            lines.emplace_back(knots[i], knots[i + 1], m, b);
+        }
+
+        return lines;
+    }
+
+    static double compute(const std::vector<LineNode>& splineTable, const double x) {
+
+        //binary search for correct line
+        int lBound = 0;
+        int uBound = (int)splineTable.size();
+        
+
+        while (true) {
+            const int currentIdx = (uBound + lBound) / 2;
+            const LineNode& node = splineTable[currentIdx];
+
+            if (x < node.minX) {
+                uBound = currentIdx;
+            }
+            else if (x > node.maxX) {
+                lBound = currentIdx;
+            }
+            else {
+                //compute linear interpolation
+                return node.m * x + node.b;
+            }
+        }
+        
+    }
+
+};
